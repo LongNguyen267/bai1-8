aea2e33a1a8b15a687f109a4e0f3b955ed500e45
BaoLongNguyen <baolong260704@gmail.com>
2025-08-02 00:17:20 +0700

    Delete API dki tai khoan directory

--- API dki tai khoan/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace API_dki_tai_khoan.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
