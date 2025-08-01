4fae973bafecbe7704ded42ab6d2b3a07b89ffea
Đăng Khoa <kakhoa1509@gmail.com>
2025-08-01 21:40:54 +0700

    khoa push bai

---
+++ API dki tai khoan/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace API_dki_tai_khoan.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaiKhoan> TaiKhoan { get; set; }
+    }
+}
---
+++ API dki tai khoan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace API_dki_tai_khoan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
